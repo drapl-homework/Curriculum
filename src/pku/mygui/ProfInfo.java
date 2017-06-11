2f5ce2b3fb7e9711a2e909583e85d0c5dc5b23e7
summercyy <summercyy@pku.edu.cn>
2017-06-11 17:09:22 +0800

    显示已存在教师信息

--- src/pku/mygui/ProfInfo.java
+++ src/pku/mygui/ProfInfo.java
@@ -28,7 +28,7 @@ public class ProfInfo extends JDialog {
 	private JTextField txt_office;
 	private JTextField txt_crs;
 	
-	public Teacher teacher;
+	private Teacher teacher; 
 
 	/**
 	 * Create the dialog.
@@ -38,8 +38,10 @@ public class ProfInfo extends JDialog {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
+		contentPanel.setVisible(true);
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		
 		{
 			JLabel lb_name = new JLabel("姓名");
 			lb_name.setBounds(23, 10, 54, 15);
@@ -63,38 +65,44 @@ public class ProfInfo extends JDialog {
 		{
 			txt_name = new JTextField();
 			txt_name.setBounds(63, 7, 87, 21);
+			if (current_teacher!=null) txt_name.setText(current_teacher.getName());
 			contentPanel.add(txt_name);
 			txt_name.setColumns(10);
 		}
 		{
 			txt_tel = new JTextField();
 			txt_tel.setBounds(63, 42, 178, 21);
+			if (current_teacher!=null) txt_tel.setText(current_teacher.getTel());
 			contentPanel.add(txt_tel);
 			txt_tel.setColumns(10);
 		}
 		{
 			txt_email = new JTextField();
 			txt_email.setBounds(63, 75, 178, 21);
+			if (current_teacher!=null) txt_email.setText(current_teacher.getMail());
 			contentPanel.add(txt_email);
 			txt_email.setColumns(10);
 		}
 		{
 			txt_office = new JTextField();
 			txt_office.setBounds(63, 119, 321, 21);
+			if (current_teacher!=null) txt_office.setText(current_teacher.getOffice());
 			contentPanel.add(txt_office);
 			txt_office.setColumns(10);
 		}
-		
-		JLabel lb_crs = new JLabel("课程");
-		lb_crs.setBounds(187, 10, 54, 15);
-		contentPanel.add(lb_crs);
-		
-		txt_crs = new JTextField();
-		txt_crs.setEditable(false);
-		txt_crs.setText(course.getName());
-		txt_crs.setBounds(224, 7, 66, 21);
-		contentPanel.add(txt_crs);
-		txt_crs.setColumns(10);
+		{
+			JLabel lb_crs = new JLabel("课程");
+			lb_crs.setBounds(187, 10, 54, 15);
+			contentPanel.add(lb_crs);
+		}
+		{
+			txt_crs = new JTextField();
+			txt_crs.setEditable(false);
+			txt_crs.setText(course.getName());
+			txt_crs.setBounds(224, 7, 66, 21);
+			contentPanel.add(txt_crs);
+			txt_crs.setColumns(10);
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -120,10 +128,10 @@ public class ProfInfo extends JDialog {
 						}
 						
 						ProfInfo.this.dispose();
-					}
+					} 
 				});
 				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				getRootPane().setDefaultButton(okButton); 
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
